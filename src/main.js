e711feaa17c2d3277aaa5cc19e79c4f85f5fe8d8
vmayta.dv5 <sdnkmp99@gmail.com>
2025-10-08 18:48:59 -0400

    complementando prueba en windows

--- src/main.js
+++ src/main.js
@@ -1,26 +1,31 @@
+
 const { invoke } = window.__TAURI__.core;
+const { listen } = window.__TAURI__.event; 
 
 
 function run_shiny_app() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  invoke("run_shiny_app")
-    .then((shiny_app_url) => {
-      console.log(shiny_app_url);
-      (async () => {
-        while (true) {
-          window.location.assign(shiny_app_url);
-          // Wait for a short period before attempting again,
-          // allowing the event loop to remain non-blocked.
-          await new Promise((resolve) => setTimeout(resolve, 1000)); // Waits for 0.2 second
-        }
-      })();
-    })
-    .catch((error) => {
-      console.error(error);
-      alert(error);
-    });
+    // Esto inicia R.exe y el temporizador/emisor de eventos en el backend.
+    invoke("run_shiny_app")
+        .then(() => {
+            // el evento 'shiny_ready' se reciba 
+            console.log("Comando run_shiny_app iniciado en Rust. Esperando evento 'shiny_ready'...");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert(error);
+      });
 }
 
+// Configura el listener de eventos
+listen('shiny_ready', (event) => {
+    const shinyUrl = event.payload;
+    console.log("Evento 'shiny_ready' recibido. Redirigiendo a:", shinyUrl);
+
+    // realizamos la redirección para cargar la interfaz.
+    window.location.assign(shinyUrl); 
+});
+
+
 window.addEventListener("DOMContentLoaded", () => {
-  run_shiny_app();
+    run_shiny_app();
 });
